ab840bad567dec5383cd4ab4b6346f8e9430fbc1
Rakibul <tanvirakibul@gmail.com>
2020-09-16 00:16:44 +0600

    added hero secton and card sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+
+
 
 function App() {
   return (
@@ -10,7 +13,7 @@ function App() {
       
       <Navbar />
       <Switch>
-        <Route path='/' exact />
+        <Route path='/' exact component={Home} />
       </Switch>
     </Router>
     </>
